5fae694f58b39c011f011dda3b54b00eb51c51df
Jake Cunningham <jakecunningham1910@gmail.com>
2024-09-29 00:10:26 +0100

    Add main page header

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .templates.home import views
+from home import views
 
 urlpatterns = [
     path('', views.index, name='home')
